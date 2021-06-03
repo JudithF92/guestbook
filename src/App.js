c73b6643434afb942a882f7a3eb5379cdd23fe97
Judith Feltl <judith.feltl@live.at>
2021-06-04 01:34:18 +0200

    added initial layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="contentWrapper">
+         <h1>Guestbook</h1>
+         <button id='newEntry'><FontAwesomeIcon icon={faPlus} /> New Entry</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
